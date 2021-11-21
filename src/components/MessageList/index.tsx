7d194b40422fc8731cc051127ab409713ca1e416
= <micaelcomp@alu.ufc.br>
2021-11-21 19:18:24 -0300

    add messages send

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ScrollView } from 'react-native';
-import { Message } from '../Message';
+import { api } from '../../services/api';
+import { Message, MessageProps } from '../Message';
+
+import { io } from 'socket.io-client';
 
 import { styles } from './styles';
 
+let messagesQueue: MessageProps[] = [];
+
+const socket = io(String(api.defaults.baseURL));
+
+socket.on('new_message', (newMessage) => {
+  messagesQueue.push(newMessage);
+});
+
 export function MessageList() {
-  const message = {
-    id: 'teste',
-    text: 'mensagem texto',
-    user: {
-      name: 'Micael',
-      avatar_url: 'https://github.com/micaelcc.png'
+  const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
+
+  useEffect(() => {
+    async function fetchMessages() {
+      const messagesResponse = await api.get<MessageProps[]>('/messages/last3');
+
+      setCurrentMessages(messagesResponse.data);
     }
-  }
+
+    fetchMessages();
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if(messagesQueue.length > 0) {
+        setCurrentMessages(prevState => [messagesQueue[0], prevState[0], prevState[2]]);
+        messagesQueue.shift();
+      }
+    }, 3000);
+
+    return () => clearInterval(timer);
+
+  }, []);
 
   return (
     <ScrollView
@@ -20,9 +46,7 @@ export function MessageList() {
       contentContainerStyle={styles.content}
       keyboardShouldPersistTaps="never"
     >
-      <Message data={message}/>
-      <Message data={message}/>
-      <Message data={message}/>
+      { currentMessages.map((message) => <Message key={message.id} data={message} />) }
     </ScrollView>
   )
 }
