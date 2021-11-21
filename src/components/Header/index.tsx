6c91970aa9750cd5ed1f67e553ef7a5e2451be47
= <micaelcomp@alu.ufc.br>
2021-11-21 19:16:37 -0300

    add sign in and sign out

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
+import { useAuth } from '../../hooks/auth';
+
 import { styles } from './styles';
 import { UserPhoto } from '../UserPhoto';
 
 import LogoSvg from '../../assets/logo.svg';
 
 export function Header() {
+  const { user, signOut } = useAuth();
   return (
     <View style={styles.container}>
       <LogoSvg />
       
         <View style={styles.logoutButton}>
-        <TouchableOpacity>
-          <Text style={styles.logoutText}>
-            Sair
-          </Text>
-        </TouchableOpacity>
-
-        <UserPhoto imageUri='https://github.com/micaelcc.png'/>
+        { user && 
+          <TouchableOpacity onPress={signOut}>
+            <Text style={styles.logoutText}>
+              Sair
+            </Text>
+          </TouchableOpacity>
+        }
+        <UserPhoto imageUri={user?.avatar_url}/>
       </View>
     </View>
   )
