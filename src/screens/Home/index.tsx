b3dce9f5ef3b1a3e11da5ae1f7a0a7116680ca2f
= <micaelcomp@alu.ufc.br>
2021-11-21 15:44:16 -0300

    added SignInBox component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { View } from 'react-native';
 
 import { Header } from '../../components/Header';
 import { MessageList } from '../../components/MessageList';
+import { SignInBox } from '../../components/SignInBox';
 import { styles } from './styles';
 
 export function Home(){
@@ -10,6 +11,7 @@ export function Home(){
     <View style={styles.container}>
       <Header />
       <MessageList />
+      <SignInBox />
     </View>
   )
 }
